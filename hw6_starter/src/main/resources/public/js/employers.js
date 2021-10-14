cc49ac722035185b191a7c47bf173b20a05ab131
kikiisong <55461623+kikiisong@users.noreply.github.com>
2021-10-13 20:46:28 -0400

    Task 1: 1 and 2

--- hw6_starter/src/main/resources/public/js/employers.js
+++ hw6_starter/src/main/resources/public/js/employers.js
@@ -1,3 +1,5 @@
+import { validateEmployerName, validateEmployerSector } from "./validate.js";
+
 function deleteEmployer(employerName) {
     fetch('http://localhost:7000/employers?name=' + employerName, {
             method: 'Delete',
@@ -10,3 +12,16 @@ Array.prototype.forEach.call(delButtons, function(button) {
     button.addEventListener('click', deleteEmployer.bind(null, button.id));
 });
 
+function addEmployer(){
+    if(validateEmployerName() && validateEmployerSector()){
+        const name = document.getElementById("name").value;
+        const sector = document.getElementById("sector").value;
+        const summary = document.getElementById("summary").value;
+        fetch('http://localhost:7000/employers?name=' + name + "&sector=" + sector + "&summary=" + summary, {
+                method: 'POST',
+            }
+        ).then(res => window.location.reload = window.location.reload(true));
+    }
+}
+
+document.getElementById("addButton").addEventListener("click", addEmployer);
--- hw6_starter/src/main/resources/public/js/validate.js
+++ hw6_starter/src/main/resources/public/js/validate.js
@@ -7,3 +7,53 @@ function validateUsername() {
         return true;
     }
 }
+
+export function validateEmployerName() {
+    let name = document.getElementById("name").value;
+    var format = /[~@$%^]/;
+    if(name.length < 2){
+        alert("Employer name cannot be less than 2 characters, more than 150 characters, or include $, @, ^, %, ~!")
+        return false;
+    }
+    else if(name.length > 150)
+    {
+        alert("Employer name cannot be less than 2 characters, more than 150 characters, or include $, @, ^, %, ~!");
+        return false;
+    }
+    else if(format.test(name))
+    {
+        alert("Employer name cannot be less than 2 characters, more than 150 characters, or include $, @, ^, %, ~!");
+        return false;
+    }
+    else{
+        return true
+    }
+}
+
+export function validateEmployerSector(){
+    let sector = document.getElementById("sector").value;
+    var format = /[~@$%^]/;
+    if(sector.length < 2){
+        alert("Employer sector cannot be less than 2 characters, more than 100 characters, or include any digits or $, @, ^, %, ~!")
+        return false;
+    }
+    else if(sector.length > 100)
+    {
+        alert("Employer sector cannot be less than 2 characters, more than 100 characters, or include any digits or $, @, ^, %, ~!");
+        return false;
+    }
+    else if(/\d/.test(sector))
+    {
+        alert("Employer sector cannot be less than 2 characters, more than 100 characters, or include any digits or $, @, ^, %, ~!");
+        return false;
+    }
+    else if(format.test(sector))
+    {
+        alert("Employer sector cannot be less than 2 characters, more than 100 characters, or include any digits or $, @, ^, %, ~!");
+        return false;
+    }
+    else{
+        return true
+    }
+
+}
